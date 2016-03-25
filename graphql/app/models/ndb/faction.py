05eeb1514caeb8c13ce9701d08a0eee982685998
Ryan Wise <ryanmwise1@gmail.com>
2016-03-24 17:24:26 -0700

    Revert "Step 6d: remove caching once more"
    This reverts commit af934c2d330316976a37d7cd7134da79f9614d1d.

--- graphql/app/models/ndb/faction.py
+++ graphql/app/models/ndb/faction.py
@@ -4,9 +4,6 @@ from .character import Character
 
 
 class Faction(ndb.Model):
-    _use_cache = False
-    _use_memcache = False
-
     name = ndb.StringProperty(required=True)
     description = ndb.TextProperty()
     created = ndb.DateTimeProperty(required=True, auto_now_add=True)
